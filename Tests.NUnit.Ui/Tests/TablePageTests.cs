b24c0cab9acf85347d1cec847ae566d1bfeba4b2
Oleh Kutafin <kutafin.o.v@gmail.com>
2024-10-08 00:27:56 -0700

    add homework 19

---
+++ Tests.NUnit.Ui/Tests/TablePageTests.cs
@@ -0,0 +1,40 @@
+using Tests.Utils.Swd.PageObjects;
+using static Tests.Utils.Swd.BaseWebElements.Browser.BrowserNames;
+
+namespace Tests.NUnit.Ui.Tests;
+
+[TestFixture]
+public class TablePageTests
+{
+    private readonly TablePage _tablePage = new ();
+    
+    [OneTimeSetUp]
+    public void OneTimeSetUp()
+    {
+        _tablePage.OpenInBrowser(Chrome, "--start-maximized");
+        _tablePage.NavigateToPage();
+    }
+    
+    [Test]
+    public void OpenWebTablePage_TitleIsCorrect()
+    {
+        var title = _tablePage.Title?.Text;
+
+        var cellKierra = _tablePage.Table?.GetCellFromRows("Kierra");
+            cellKierra?.Click();
+        var cellKierraText = cellKierra?.Text;
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(title, Is.EqualTo("Web Tables"));
+            Assert.That(cellKierra, Is.Not.Null);
+            Assert.That(cellKierraText, Is.EqualTo("Kierra"));
+        });
+    }
+    
+    [OneTimeTearDown]
+    public void OneTimeTearDown()
+    {
+        _tablePage.Close();
+    }
+}
---
+++ Tests.Utils.Swd/PageObjects/TablePage.cs
@@ -0,0 +1,36 @@
+using Tests.Utils.Swd.Attribute;
+using Tests.Utils.Swd.BaseWebElements.Browser;
+using Tests.Utils.Swd.BaseWebElements.Elements;
+using Tests.Utils.Swd.BaseWebElements.Elements.Table;
+using Tests.Utils.Swd.BaseWebElements.Page.Abstractions;
+
+namespace Tests.Utils.Swd.PageObjects;
+
+public class TablePage : BasePage
+{
+    private string Url => "https://demoqa.com/webtables";
+    
+    [FindBy(XPath = "//h1[contains(text(),\"Web Tables\")]")]
+    public Element? Title { get; set; }
+
+    [FindBy(XPath = "//div[@class='ReactTable -striped -highlight']")]
+    public Table? Table { get; set; }
+
+    public TablePage OpenInBrowser(BrowserNames name, params string[] args)
+    {
+        OpenWith(name, args);
+        return this;
+    }
+
+    public TablePage NavigateToPage()
+    {
+        NavigateTo(Url);
+        return this;
+    }
+
+    public TablePage ClickOnRowWithText(string text)
+    {
+         Table?.GetCellFromRows(text).Click();
+         return this;
+    }
+}
