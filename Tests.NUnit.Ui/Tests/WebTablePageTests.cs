2f1a53fa6ff1346230f006fcde9c585082a7343e
Sergii Zvonov <s.zvonov@gmail.com>
2024-10-20 18:15:47 +0300

    implement webtable tests

--- Tests.NUnit.Ui/Tests/WebTablePageTests.cs
+++ Tests.NUnit.Ui/Tests/WebTablePageTests.cs
@@ -1,17 +1,15 @@
-using Tests.Utils.Swd.Helpers;
 using Tests.Utils.Swd.PageObjects;
 using static Tests.Utils.Swd.BaseWebElements.Browser.BrowserNames;
 
 namespace Tests.NUnit.Ui.Tests;
 
-//TODO: finish the test as described in homework   
 [TestFixture]
 public class WebTablePageTests
 {
     private readonly WebTablePage _webTablePage = new();
 
-    [OneTimeSetUp]
-    public void OneTimeSetUp()
+    [SetUp]
+    public void SetUp()
     {
         _webTablePage.OpenInBrowser(Chrome, "--start-maximized");
         _webTablePage.NavigateToPage();
@@ -38,8 +36,91 @@ public class WebTablePageTests
         });
     }
 
-    [OneTimeTearDown]
-    public void OneTimeTearDown()
+    [Test]
+    public void AddElementToTable()
+    {
+        //Arrange
+        var initialRows = _webTablePage.Table?.FindRows().GetAll();
+        var addDisplayed = _webTablePage.Add.IsDisplayed();
+        var addEnabled = _webTablePage.Add.IsEnabled();
+
+        //Act
+        _webTablePage.Add.Click();
+        _webTablePage.FillOutRegistrationForm();
+        var submitDisplayed = _webTablePage.Submit.IsDisplayed();
+        var submitEnabled = _webTablePage.Submit.IsEnabled();
+        _webTablePage.Submit.Click();
+        var rows = _webTablePage.Table?.FindRows().GetAll();
+        var rowCells = rows?[3].FindCells().GetAll();
+        var isNewRowAdded = rows.Count - initialRows.Count == 1;
+
+        //Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(addDisplayed, Is.True);
+            Assert.That(addEnabled, Is.True);
+            Assert.That(submitDisplayed, Is.True);
+            Assert.That(submitEnabled, Is.True);
+            Assert.That(isNewRowAdded, Is.True);
+            Assert.That(rowCells?[0].GetText(), Is.EqualTo(_webTablePage.firstName));
+            Assert.That(rowCells?[1].GetText(), Is.EqualTo(_webTablePage.lastName));
+            Assert.That(rowCells?[2].GetText(), Is.EqualTo(_webTablePage.age));
+            Assert.That(rowCells?[3].GetText(), Is.EqualTo(_webTablePage.email));
+            Assert.That(rowCells?[4].GetText(), Is.EqualTo(_webTablePage.salary));
+            Assert.That(rowCells?[5].GetText(), Is.EqualTo(_webTablePage.department));            
+        });
+    }
+
+    [Test]
+    public void DeleteElementFromTable()
+    {
+        //Arrange
+        var initialRows = _webTablePage.Table?.FindRows().GetAll();
+        var deleteDisplayed = _webTablePage.Delete.IsDisplayed();
+        var deleteEnabled = _webTablePage.Delete.IsEnabled();
+
+        //Act
+        _webTablePage.Delete.Click();
+        var rows = _webTablePage.Table?.FindRows().GetAll();
+        var isNewRowDeleted = initialRows.Count - rows.Count == 1;
+
+        //Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(deleteDisplayed, Is.True);
+            Assert.That(deleteEnabled, Is.True);            
+            Assert.That(isNewRowDeleted, Is.True);                        
+        });
+    }
+
+    [Test]
+    public void SortingTableByAge()
+    {
+        //Arrange
+        _webTablePage.AgeColumnHeader.Click();
+        var columnsAscending = _webTablePage.Table?.FindColumns().GetAll();
+        var columnCellsAscending = columnsAscending?[2].FindCells().GetAll();
+
+        //Act
+        var intListAscending = WebTablePage.ConvertToIntegerList(columnCellsAscending);
+        var isAscending = _webTablePage.IsSortedAscending(intListAscending);
+
+        _webTablePage.AgeColumnHeader.Click();
+        var columnsDescending = _webTablePage.Table?.FindColumns().GetAll();
+        var columnCellsDescending = columnsDescending?[2].FindCells().GetAll();
+        var intListDescending = WebTablePage.ConvertToIntegerList(columnCellsDescending);
+        var isDescending = _webTablePage.IsSortedDescending(intListDescending);
+
+        //Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(isAscending, Is.True);
+            Assert.That(isDescending, Is.True);                                   
+        });
+    }
+
+    [TearDown]
+    public void TearDown()
     {
         _webTablePage.Close();
     }
--- Tests.Utils.Swd/PageObjects/WebTablePage.cs
+++ Tests.Utils.Swd/PageObjects/WebTablePage.cs
@@ -16,6 +16,46 @@ public class WebTablePage : BasePage
     [FindBy(XPath = "//div[@class='rt-tbody']")]
     public Table? Table { get; set; }
 
+    [FindBy(Id = "delete-record-1")]
+    public Button Delete { get; set; }
+
+    [FindBy(CssSelector = ".ReactTable .rt-thead .rt-th:nth-child(3)")]
+    public Element AgeColumnHeader { get; set; }
+
+    [FindBy(Id = "addNewRecordButton")]
+    public Button? Add { get; set; }
+
+    [FindBy(Id = "submit")]
+    public Button? Submit { get; set; }
+
+    [FindBy(Id = "registration-form-modal")]
+    public Element RegistrationForm {get; set; }
+
+    [FindBy(Id = "firstName")]
+    private Input FirstNameInput {get; set; }
+
+    [FindBy(Id = "lastName")]
+    private Input LastNameInput {get; set; }
+
+    [FindBy(Id = "userEmail")]
+    private Input EmailInput {get; set; }
+
+    [FindBy(Id = "age")]
+    private Input AgeInput {get; set; }
+
+    [FindBy(Id = "salary")]
+    private Input SalaryInput {get; set; }
+
+    [FindBy(Id = "department")]
+    private Input DepartmentInput {get; set; }
+
+    public string firstName = "Firstname";
+    public string lastName = "Lastname";
+    public string email = "email@email.com";
+    public string age = "0";
+    public string salary = "0";
+    public string department = "Department";
+
     public WebTablePage OpenInBrowser(BrowserNames name, params string[] args)
     {
         OpenWith(name, args);
@@ -27,4 +67,41 @@ public class WebTablePage : BasePage
         NavigateTo(Url);
         return this;
     }
+
+    public void FillOutRegistrationForm()
+    {
+        FirstNameInput.SendKeys(firstName);
+        LastNameInput.SendKeys(lastName);
+        EmailInput.SendKeys(email);
+        AgeInput.SendKeys(age);
+        SalaryInput.SendKeys(salary);
+        DepartmentInput.SendKeys(department);
+    }
+
+    public static List<int> ConvertToIntegerList(IList<Element> list)
+    {
+        var intList = new List<int>();
+
+        for (var i = 0; i < list.Count; i++)
+        {
+            var cellText = list[i].GetText();
+
+            if (int.TryParse(cellText, out int cellInt))
+            {
+                intList.Add(cellInt);
+            }
+        }
+
+        return intList;
+    }
+
+    public bool IsSortedAscending(List<int> list)
+    {
+        return list.SequenceEqual(list.OrderBy(x => x));
+    }
+
+    public bool IsSortedDescending(List<int> list)
+    {
+        return list.SequenceEqual(list.OrderByDescending(x => x));
+    }   
 }
