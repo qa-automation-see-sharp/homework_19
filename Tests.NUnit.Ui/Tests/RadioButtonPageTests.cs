f06f5cf35d26e097752277b4c8d6bc7bdd05bc63
Liudmyla <savinska.l.y@gmail.com>
2024-11-01 13:52:34 +0100

    Added RadioButtonsPageTests

--- Tests.NUnit.Ui/Tests/RadioButtonPageTests.cs
+++ Tests.NUnit.Ui/Tests/RadioButtonPageTests.cs
@@ -1,7 +1,62 @@
+using Tests.Utils.Swd.BaseWebElements.Browser;
+using Tests.Utils.Swd.PageObjects;
+
 namespace Tests.NUnit.Ui.Tests;
 
-//TODO: cover with tests
+[TestFixture]
 public class RadioButtonPageTests
 {
+    private RadioButtonPage _radioButtonPage;
+
+    [OneTimeSetUp]
+    public void OneTimeSetUp()
+    {
+        _radioButtonPage = new RadioButtonPage();
+        _radioButtonPage.OpenInBrowser(BrowserNames.Chrome, "--start-maximized");
+        _radioButtonPage.NavigateToPage();
+    }
     
+    [Test, Order(1)]
+    [Description("This test checks if the user has landed to the page with the correct title")]
+    public void OpenRadioButtonPage_TitleIsCorrect()
+    {
+        var title = _radioButtonPage.Title?.GetText();
+
+        Assert.That(title, Is.EqualTo("Radio Button"));
+    }
+
+    [Test, Order(2)]
+    [Description("This test checks if the Yes Radio button is clicked and the correct message is displayed")]
+    public void ClickYesRadioButton_DisplayMessage()
+    {
+        _radioButtonPage.ClickYesRadioButton();
+        var displayedMessage = _radioButtonPage.YouHaveSelectedYesMessage();
+        
+        Assert.That(displayedMessage, Is.True);
+    }
+
+    [Test, Order(3)]
+    [Description("This test checks if the Impressive Radio button is clicked and the correct message is displayed")]
+    public void ClickImpressedRadioButton_DisplayMessage()
+    {
+        _radioButtonPage.ClickImpressiveRadioButton();
+        var displayedMessage = _radioButtonPage.YouHaveSelectedImpressiveMessage();
+        
+        Assert.That(displayedMessage, Is.True);
+    }
+
+    [Test, Order(4)]
+    [Description("This test checks that the No Radio button is disabled")]
+    public void CheckNoRadioButtonIsDisabled_ReturnTrue()
+    {
+        var isDisabledButton = _radioButtonPage.CheckNoRadioButton();
+        
+        Assert.That(isDisabledButton, Is.True);
+    }
+    
+    [OneTimeTearDown]
+    public void OneTimeTearDown()
+    {
+        _radioButtonPage.Close();
+    }
 }
