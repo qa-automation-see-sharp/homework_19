98864c47827576f4d209b6822162bacbebae6524
Ksenia <okstrishch@gmail.com>
2024-11-10 17:35:06 +0200

    table tests

--- Tests.NUnit.Ui/Tests/WebTablePageTests.cs
+++ Tests.NUnit.Ui/Tests/WebTablePageTests.cs
@@ -9,8 +9,9 @@ namespace Tests.NUnit.Ui.Tests;
 public class WebTablePageTests
 {
     private readonly WebTablePage _webTablePage = new();
+    private readonly RegistrationFormsWindow _registrationFormsWindow = new();
 
-    [OneTimeSetUp]
+    [SetUp]
     public void OneTimeSetUp()
     {
         _webTablePage.OpenInBrowser(Chrome, "--start-maximized");
@@ -38,8 +39,63 @@ public class WebTablePageTests
         });
     }
 
-    [OneTimeTearDown]
-    public void OneTimeTearDown()
+    [Test]
+    public void AddUserToTable()
+    {
+        var initialRows = _webTablePage.Table?.FindRows().GetAll();
+
+        _webTablePage.ClickAddButton().AddUser("Natali", "Black", "natali.black@gmail.com", 18, 2000, "Telecom");
+        var rows = _webTablePage.Table?.FindRows().GetAll();
+        var rowCells = rows?[rows.Count-1].FindCells().GetAll();
+        var isNewRowAdded = rows!.Count - initialRows!.Count == 1;
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(isNewRowAdded, Is.True);
+            Assert.That(rowCells?[0].GetText(), Is.EqualTo("Natali"));
+            Assert.That(rowCells?[1].GetText(), Is.EqualTo("Black"));
+            Assert.That(rowCells?[2].GetText(), Is.EqualTo("18"));
+            Assert.That(rowCells?[3].GetText(), Is.EqualTo("natali.black@gmail.com"));
+            Assert.That(rowCells?[4].GetText(), Is.EqualTo("2000"));
+            Assert.That(rowCells?[5].GetText(), Is.EqualTo("Telecom"));            
+        });
+    }
+
+    [Test]
+    public void DeleteUserFromTable()
+    {
+        var initialRows = _webTablePage.Table?.FindRows().GetAll();
+
+        _webTablePage.Delete.Click();
+        var rows = _webTablePage.Table?.FindRows().GetAll();
+        var isNewRowDeleted = initialRows!.Count - rows!.Count == 1;
+
+        Assert.Multiple(() =>
+        {           
+            Assert.That(isNewRowDeleted, Is.True);                        
+        });
+    }
+
+    [Test]
+    public void SortingTableByAge()
+    {
+        var columnsAscending = _webTablePage.Table?.FindColumns().GetAll();
+
+        var orderedList = columnsAscending?[2].FindCells().GetAll();
+        List<string> expectedData = WebTablePage.DataSorting(orderedList).OrderBy(x => x).ToList();
+        _webTablePage.AgeColumnHeader.Click();
+        var columnCellsAscending = columnsAscending?[2].FindCells().GetAll();
+        List<string> dataAfterSorting = WebTablePage.DataSorting(columnCellsAscending);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(expectedData[0].Equals(dataAfterSorting[0]));
+            Assert.That(expectedData[1].Equals(dataAfterSorting[1]));                               
+        });
+    }
+
+    [TearDown]
+    public void TearDown()
     {
         _webTablePage.Close();
     }
--- Tests.Utils.Swd/PageObjects/WebTablePage.cs
+++ Tests.Utils.Swd/PageObjects/WebTablePage.cs
@@ -1,3 +1,4 @@
+using OpenQA.Selenium;
 using Tests.Utils.Swd.Attribute;
 using Tests.Utils.Swd.BaseWebElements.Browser;
 using Tests.Utils.Swd.BaseWebElements.Elements;
@@ -16,6 +17,16 @@ public class WebTablePage : BasePage
     [FindBy(XPath = "//div[@class='rt-tbody']")]
     public Table? Table { get; set; }
 
+    [FindBy(Id = "addNewRecordButton")]
+    public Button? Add { get; set; }
+
+    [FindBy(CssSelector = ".ReactTable .rt-thead .rt-th:nth-child(3)")]
+    public Element AgeColumnHeader { get; set; }
+
+    [FindBy(XPath ="//*[starts-with(@id,'delete-record')]")]
+    public Element Delete {get; set;}
+    
+
     public WebTablePage OpenInBrowser(BrowserNames name, params string[] args)
     {
         OpenWith(name, args);
@@ -26,5 +37,32 @@ public class WebTablePage : BasePage
     {
         NavigateTo(Url);
         return this;
+    }
+
+    public bool IsSortedAscending(List<int> list)
+    {
+        return list.SequenceEqual(list.OrderBy(x => x));
+    }
+
+    public bool IsSortedDescending(List<int> list)
+    {
+        return list.SequenceEqual(list.OrderByDescending(x => x));
+    }   
+
+    public RegistrationFormsWindow ClickAddButton()
+    {
+        Add.Click();
+        return new RegistrationFormsWindow();
+    }
+
+    public static List<string> DataSorting(IList<Element> list)
+    {
+        var dataSorting = new List<string>();
+
+        foreach (var element in list)
+        {
+            dataSorting.Add(element.GetText());
+        }
+        return dataSorting;
     }
 }
